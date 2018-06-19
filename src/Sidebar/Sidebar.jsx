64fc791f60075560591af37fe428ee93c8fc7fa9
Rainer Simon <rainer.simon@ait.ac.at>
2018-06-19 10:12:12 +0200

    Sidebar slide in/out

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 export default class Sidebar extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+  }
+
   toggle() {
-    console.log('toggle sidebar');
+    this.setState(previous => ({
+      visible: !previous.visible
+    }));
   }
 
   render() {
     return (
-      <div className="sidebar">
-        <div className="content">
-          {/*
-          <SearchBox>
-            <!-- TODO -->
-          </SearchBox>
-          <PersonList>
-            <!-- TODO -->s
-          </PersonList>
-          */}
+      <CSSTransition in={this.state.visible} timeout={300} classNames="slide">
+        <div className="sidebar">
+          <div className="content">
+            {/*
+            <SearchBox>
+              <!-- TODO -->
+            </SearchBox>
+            <PersonList>
+              <!-- TODO -->s
+            </PersonList>
+            */}
+          </div>
+          <div className="tab">
+            <button onClick={this.toggle.bind(this)}>&#xf002;</button>
+          </div>
         </div>
-        <div className="tab">
-          <button onClick={this.toggle}>&#xf002;</button>
-        </div>
-      </div>
+      </CSSTransition>
     )
   }
 }
