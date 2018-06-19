5ba33671a421f08e85e1cbfaa70413423f33d9fb
Rainer Simon <rainer.simon@ait.ac.at>
2018-06-19 13:57:39 +0200

    Minor tweak

--- src/Sidebar/components/SearchBox.jsx
+++ src/Sidebar/components/SearchBox.jsx
@@ -4,7 +4,7 @@ export default class SearchBox extends Component {
   render() {
     return (
       <div className="searchbox">
-        <input type="text" placeholder="Find Person..."/>
+        <input type="text" placeholder="Search..."/>
       </div>
     )
   }
