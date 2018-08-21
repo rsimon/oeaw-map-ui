58cda9f8106794f1c312bb64f39558e9e1fbb092
Rainer Simon <rainer.simon@ait.ac.at>
2018-08-21 13:59:37 +0200

    Minor bugfix

--- src/Map/MarkerLayer/SelectableMarker.jsx
+++ src/Map/MarkerLayer/SelectableMarker.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Marker, CircleMarker, Popup } from 'react-leaflet';
 
 // Preload images
-['/public/images/marker-disabled.png', '/public/images/marker-selected.png'].forEach(url => {
+['public/images/marker-disabled.png', 'public/images/marker-selected.png'].forEach(url => {
   const preloaded = new Image();
   preloaded.src = url;
 });
@@ -11,22 +11,22 @@ import { Marker, CircleMarker, Popup } from 'react-leaflet';
 const ICON_DEFAULTS = {
   iconSize     : [25, 41],
   iconAnchor   : [12, 41],
-  shadowUrl    : '/public/images/marker-shadow.png',
+  shadowUrl    : 'public/images/marker-shadow.png',
   shadowSize   : [41, 41],
   shadowAnchor : [12, 41],
   popupAnchor  : [0, -30]
 }
 
 const DEFAULT_ICON = L.icon(Object.assign({
-  iconUrl: '/public/images/marker.png'
+  iconUrl: 'public/images/marker.png'
 }, ICON_DEFAULTS));
 
 const DISABLED_ICON = L.icon(Object.assign({
-  iconUrl: '/public/images/marker-disabled.png'
+  iconUrl: 'public/images/marker-disabled.png'
 }, ICON_DEFAULTS));
 
 const SELECTED_ICON = L.icon({
-  iconUrl     : '/public/images/marker-selected.png',
+  iconUrl     : 'public/images/marker-selected.png',
   iconSize    : [53, 65],
   iconAnchor  : [24, 42],
   popupAnchor : [0, -50]
