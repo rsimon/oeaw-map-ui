4c369d6fc1b0f11d4d4139f24f3ae860bcfb7b08
Rainer Simon <rainer.simon@ait.ac.at>
2018-08-21 13:54:28 +0200

    Added image preloading

--- src/Map/MarkerLayer/SelectableMarker.jsx
+++ src/Map/MarkerLayer/SelectableMarker.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 import { Marker, CircleMarker, Popup } from 'react-leaflet';
 
+// Preload images
+['/public/images/marker-disabled.png', '/public/images/marker-selected.png'].forEach(url => {
+  const preloaded = new Image();
+  preloaded.src = url;
+});
+
 const ICON_DEFAULTS = {
   iconSize     : [25, 41],
   iconAnchor   : [12, 41],
@@ -27,6 +33,10 @@ const SELECTED_ICON = L.icon({
 });
 
 export default class SelectableMarker extends Component {
+
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
